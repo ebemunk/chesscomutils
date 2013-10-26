518cac4ba178697cdb365e058d10251ade659a7c
ebemunk <ebemunk@gmail.com>
2013-10-26 16:41:18 -0700

    relocate openings dir

---
+++ scid-openings/extract_fen.js
@@ -0,0 +1,18 @@
+// extract FEN positions from the openings to construct openings_fen.js
+function extract_fen() {
+	var obj = {};
+	$.each(CC.openings, function(key, val) {
+		var moves = val.moves.replace(/[0-9]\./g, '').split(' ');
+		op = new Chess();
+		$.each(moves, function(i, v) {
+			op.move(v);
+		});
+		val.fen = op.fen(); //.replace(/ (w|b) (-|[0-9a-zA-z]*) (-|[0-9a-zA-z]*) [0-9]* [0-9]*/, '');
+		obj[val.fen] = {
+			eco: val.eco,
+			name: val.name,
+			moves: val.moves
+		};
+	});
+	console.log(Object.keys(obj).length);
+}
