99565f33a76e4a02f0bb1ea4e90ff8f97d5b45e7
ebemunk <ebemunk@gmail.com>
2013-11-28 22:04:31 -0800

    fix bugs in fen conversion regex

--- scid-openings/extract_fen.js
+++ scid-openings/extract_fen.js
@@ -1,19 +1,31 @@
 // extract FEN positions from the openings to construct openings_fen.js
 function extract_fen() {
 	var obj = {};
-	$.each(CC.openings, function(key, val) {
-		var moves = val.moves.replace(/[0-9]\./g, '').split(' ');
+	var openings_size = 0, key;
+	for (key in CC.openings) {
+		if (CC.openings.hasOwnProperty(key)) openings_size++;
+	}
+	console.log('starting, ' + openings_size + ' openings to process.');
+
+	var count = 0;
+	for(key in CC.openings) {
+		count++;
+		// if(count < 8000) continue;
+		var val = CC.openings[key];
+		console.log('0');
+
+		var moves = val.moves.replace(/[0-9]*\. ?/g, '').split(' ');
 		op = new Chess();
-		$.each(moves, function(i, v) {
-			op.move(v);
-		});
-		val.fen = op.fen(); //.replace(/ (w|b) (-|[0-9a-zA-z]*) (-|[0-9a-zA-z]*) [0-9]* [0-9]*/, '');
-		// obj[val.fen] = {
-		// 	eco: val.eco,
-		// 	name: val.name,
-		// 	moves: val.moves
-		// };
+		for(v in moves) {
+			if(!op.move(moves[v])) {
+				console.log('wrong move at: ' + moves[v] + '!', moves, val.moves);
+				return;
+			}
+		}
+		val.fen = op.fen();
 		obj[val.fen] = key;
-	});
+		index++;
+	}
+	console.log('writing to doc');
 	document.write(JSON.stringify(obj));
 }
