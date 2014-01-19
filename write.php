b29da6ebe2bcbd83fe5104702df87d747cbea6fe
ebemunk <ebemunk@gmail.com>
2014-01-19 09:19:26 -0800

    separate out filename as parameter

--- write.php
+++ write.php
@@ -4,5 +4,7 @@
 //allow requests from live.chess.com where our script is injected
 header("Access-Control-Allow-Origin: http://live.chess.com");
 
+$folder = "C:\\Users\\derpatron\\Desktop\\";
+$location = (isset($_POST['filename']) ? $_POST['filename'] : 'chessdb.pgn');
 //write pgn to file. create file if it doesnt exist and append to it if it does
-file_put_contents('C:\\Users\\derpatron\\Desktop\\chessdb.pgn', $_POST['pgn'] . PHP_EOL . PHP_EOL, FILE_APPEND);
+file_put_contents($folder.$location, $_POST['pgn'] . PHP_EOL . PHP_EOL, FILE_APPEND);
