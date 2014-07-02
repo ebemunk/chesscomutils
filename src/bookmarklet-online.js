ddb99367c4db3620339889788406be2081a72f76
ebemunk <ebemunk@gmail.com>
2014-07-01 20:19:34 -0700

    change link

--- src/bookmarklet-online.js
+++ src/bookmarklet-online.js
@@ -51,7 +51,7 @@
 		id: 'openings'
 	},
 	{
-		url: 'http://direct.ebemunk.com/chesscomutils/ccutils-bundled.min.js',
+		url: 'http://ebemunk.com/chesscomutils/ccutils-bundled.min.js',
 		id: 'ccutils'
 	}
 ]);
